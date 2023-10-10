85d786b893e4929bf90b2a6f606361d14497fa26
Humberto Anjos <h.anjos@sbrubbles.org>
2023-10-09 22:25:18 -0300

    More tests.

--- test/org/sbrubbles/conditio_test.clj
+++ test/org/sbrubbles/conditio_test.clj
@@ -5,7 +5,9 @@
     [clojure.test.check.clojure-test :refer [defspec]]
     [clojure.test.check.generators :as gen]
     [clojure.test.check.properties :as prop]
-    [org.sbrubbles.conditio :as c]))
+    [org.sbrubbles.conditio :as c])
+  (:import
+    (clojure.lang ExceptionInfo)))
 
 (deftest signal-test
   (testing "signal calls the handler it gets if it finds one"
@@ -13,7 +15,7 @@
       (is (= (c/signal :test 1) 2))))
 
   (testing "the default behavior is to explode if given an unknown condition"
-    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Handler not found"
+    (is (thrown-with-msg? ExceptionInfo #"Handler not found"
                           (c/signal :nonexistent 1 2 3))))
 
   (testing "signal signals ::c/handler-not-found if it doesn't find the given one"
@@ -27,7 +29,7 @@
              (inc 1)))))
 
   (testing "the default behavior is to explode if given an unknown restart"
-    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"Restart not found"
+    (is (thrown-with-msg? ExceptionInfo #"Restart not found"
                           ((c/use-restart :nonexistent)))))
 
   (testing "use-restart signals ::c/restart-not-found if it doesn't find the given restart"
@@ -35,14 +37,21 @@
       (is (= ((c/use-restart :nonexistent 1))
              :test)))))
 
+(deftest abort-test
+  (testing "abort returns a function which explodes when called"
+    (is (thrown? ExceptionInfo
+                 ((c/abort))))))
+
 (defspec handle-registers-handlers-only-in-its-context
   100
   (prop/for-all [id (gen/such-that #(not (#{::c/handler-not-found ::c/restart-not-found} %))
                                    gen/any-equatable)
                  value gen/any]
     (is (not (c/*handlers* id)))
+
     (c/handle [id value]
       (is (= (c/*handlers* id) value)))
+
     (is (not (c/*handlers* id)))))
 
 (defspec with-restart-registers-restarts-only-in-its-context
@@ -50,17 +59,22 @@
   (prop/for-all [id gen/any-equatable
                  value gen/any]
     (is (not (c/*restarts* id)))
+
     (c/with-restarts [id value]
       (is (= (c/*restarts* id) value)))
+
     (is (not (c/*restarts* id)))))
 
-(defspec abort-takes-anything-as-message
+(defspec with-restart-fn-works-just-like-with-restarts
   100
-  (prop/for-all [v gen/any]
-    (try
-      ((c/abort v))
-      (catch clojure.lang.ExceptionInfo e
-        (is (str v) (.getMessage e))))))
+  (prop/for-all [id gen/any-equatable
+                 value gen/any-equatable]
+    (let [f (c/use-restart id)
+          with-restarts-f (c/with-restarts-fn f {id (fn [] value)})]
+      (c/with-restarts [id (fn [] value)]
+        (is (= (f) value)))
+
+      (is (= (with-restarts-f) value)))))
 
 
 
