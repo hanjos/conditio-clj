fc282b1761f263d6313767134b7ed256a2437312
Humberto Anjos <h.anjos@sbrubbles.org>
2023-10-16 22:59:43 -0300

    Putting more data in metadata.

--- build.clj
+++ build.clj
@@ -7,93 +7,106 @@
 
 ; coordinates
 (def metadata
-  {:lib      'org.sbrubbles/conditio-clj
-   :version  "0.1.0"
-   :pom-data [[:description "A simple condition system for Clojure, without too much machinery."]
-              [:url "https://github.com/hanjos/conditio-clj"]
-              [:licenses
-               [:license
-                [:name "MIT License"]
-                [:url "https://github.com/hanjos/conditio-clj/blob/main/LICENSE"]]]
-              [:distributionManagement
-               [:repository
-                [:id "github"]
-                [:name "GitHub hanjos Apache Maven Packages"]
-                [:url "https://maven.pkg.github.com/hanjos/conditio-clj"]]]]})
+  {:lib          'org.sbrubbles/conditio-clj
+   :version      "0.1.0"
+   :dirs         {:src     "src"
+                  :target  "target"
+                  :classes "target/classes"
+                  :doc     "target/doc"}
+   :basis        (b/create-basis {:project "deps.edn"})
+   :ignore-files [#"user.clj"]
+   :pom-data     [[:description "A simple condition system for Clojure, without too much machinery."]
+                  [:url "https://github.com/hanjos/conditio-clj"]
+                  [:licenses
+                   [:license
+                    [:name "MIT License"]
+                    [:url "https://github.com/hanjos/conditio-clj/blob/main/LICENSE"]]]
+                  [:distributionManagement
+                   [:repository
+                    [:id "github"]
+                    [:name "GitHub hanjos Apache Maven Packages"]
+                    [:url "https://maven.pkg.github.com/hanjos/conditio-clj"]]]]})
 
-; directories
-(def src-dir "src")
-(def dist-dir "target")
-(def class-dir (str dist-dir "/classes"))
-(def doc-dir (str dist-dir "/doc"))
-(def basis (b/create-basis {:project "deps.edn"}))
-
-; files
-(def ignore-files [#"user.clj"])
-(def jar-file (format (str dist-dir "/%s-%s.jar")
-                      (name (:lib metadata))
-                      (:version metadata)))
+(def jar-file
+  (let [{:keys [lib version dirs]} metadata
+        {:keys [target]} dirs]
+    (format (str target "/%s-%s.jar")
+            (name lib)
+            version)))
 
 ; helper functions
-(defn- echo [opts & [args]]
-  (when (:verbose opts) (println (apply str args))))
+(def flags
+  (into {} ; filtering out nil values
+        (filter second)
+        {:verbose (System/getenv "CONDITIO_VERBOSE")}))
+
+(defn echo [& args]
+  (when (:verbose flags) (println (apply str args))))
 
 ; available commands
 (defn clean [opts]
-  (echo opts "Cleaning " dist-dir "...")
-  (b/delete {:path dist-dir})
+  (let [{{:keys [target]} :dirs} metadata]
+    (echo "Cleaning " target "...")
+    (b/delete {:path target}))
 
   opts)
 
 (defn jar [opts]
-  (echo opts "Writing POM...")
-  (let [{:keys [lib version pom-data]} metadata]
-    (b/write-pom {:class-dir class-dir
+  (let [{:keys [lib version pom-data basis dirs ignore-files]} metadata
+        {:keys [src classes]} dirs]
+    (echo "Writing POM...")
+    (b/write-pom {:class-dir classes
                   :lib       lib
                   :version   version
                   :basis     basis
-                  :src-dirs  [src-dir]
-                  :pom-data  pom-data}))
+                  :src-dirs  [src]
+                  :pom-data  pom-data})
 
-  (echo opts "Copying sources...")
-  (b/copy-dir {:src-dirs   [src-dir]
-               :target-dir class-dir
-               :ignores    ignore-files})
+    (echo "Copying sources...")
+    (b/copy-dir {:src-dirs   [src]
+                 :target-dir classes
+                 :ignores    ignore-files})
 
-  (echo opts "Creating the JAR...")
-  (b/jar {:class-dir class-dir
-          :jar-file  jar-file})
+    (echo "Creating the JAR...")
+    (b/jar {:class-dir classes
+            :jar-file  jar-file}))
 
   opts)
 
 (defn doc [opts]
-  (echo opts "Generating docs...")
-  (let [{:keys [lib version]} metadata
+  (let [{:keys [lib version dirs]} metadata
+        {:keys [src doc]} dirs
         description (get-in metadata [:pom-data 0 1])]
+    (echo "Generating docs...")
     (codox/generate-docs {:name         (name lib)
                           :version      version
                           :description  description
                           :language     :clojure
-                          :output-path  doc-dir
-                          :source-paths [src-dir]
+                          :output-path  doc
+                          :source-paths [src]
                           :namespaces   ['org.sbrubbles.conditio]
                           :exclude-vars #"^(map)?->\p{Upper}"
                           :metadata     {:doc/format :markdown}
-                          :themes       [:default]})))
+                          :themes       [:default]}))
+
+  opts)
 
 (defn deploy [opts]
-  (echo opts "Deploying...")
-  (let [{:keys [lib version]} metadata
-        repoUrl (get-in metadata [:pom-data 3 1 3 1])]
+  (let [{:keys [lib version dirs]} metadata
+        {:keys [classes]} dirs
+        repo-url (get-in metadata [:pom-data 3 1 3 1])]
+    (echo "Deploying...")
     (aether/deploy :coordinates [lib version]
                    :jar-file (io/file jar-file)
-                   :pom-file (io/file (str class-dir
+                   :pom-file (io/file (str classes
                                            "/META-INF/maven/"
                                            lib
                                            "/pom.xml"))
-                   :repository {:url      repoUrl
+                   :repository {:url      repo-url
                                 :username (System/getenv "USERNAME")
-                                :password (System/getenv "TOKEN")})))
+                                :password (System/getenv "TOKEN")}))
+
+  opts)
 
 (defn version [opts]
   (println (:version metadata))
